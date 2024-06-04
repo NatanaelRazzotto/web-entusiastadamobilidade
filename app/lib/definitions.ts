e8e4228c195ef6213d2171bfb3200e3e9225a9ea
NATANAEL RAZZOTTO <natanael8447@outlook.com>
2024-06-03 22:40:00 -0300

    feat: :sparkles: Inclusao de integracao db

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -1,13 +1,15 @@
+import prisma from 'prisma';
+
 // This file contains type definitions for your data.
 // It describes the shape of the data, and what data type each property should accept.
 // For simplicity of teaching, we're manually defining these types.
 // However, these types are generated automatically if you're using an ORM such as Prisma.
-export type User = {
-  id: string;
-  name: string;
-  email: string;
-  password: string;
-};
+// export type User = {
+//   id: string;
+//   name: string;
+//   email: string;
+//   password: string;
+// };
 
 export type Customer = {
   id: string;
@@ -86,3 +88,8 @@ export type InvoiceForm = {
   amount: number;
   status: 'pending' | 'paid';
 };
+
+///
+
+export type User = prisma.User;
+export type Post = prisma.Post;
