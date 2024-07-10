5f5483d08e34a8db7f429c80fe8bae8231f29028
NATANAEL RAZZOTTO <natanael8447@outlook.com>
2024-07-10 03:57:34 -0300

    feat: :sparkles: Migrations from data base

---
+++ prisma/migrations/20240710060520_init/migration.sql
@@ -0,0 +1,228 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "namePost" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "category" DOUBLE PRECISION NOT NULL,
+    "content" TEXT,
+    "resume" TEXT,
+    "tagPost" TEXT,
+    "coverURL" TEXT,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "authorId" TEXT,
+    "newspaperColumnID" TEXT,
+    "dateCreate" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Image" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "pathURL" TEXT,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "authorId" TEXT,
+
+    CONSTRAINT "Image_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Vehicle" (
+    "id" TEXT NOT NULL,
+    "plate" TEXT,
+    "serialNumber" TEXT,
+    "operatingCategory" DOUBLE PRECISION NOT NULL,
+    "bodyworkID" TEXT,
+    "powertrainID" TEXT,
+    "operatorID" TEXT,
+
+    CONSTRAINT "Vehicle_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Bodywork" (
+    "id" TEXT NOT NULL,
+    "nameModel" TEXT NOT NULL,
+    "description" TEXT,
+    "serialNumber" TEXT,
+    "year" DOUBLE PRECISION NOT NULL,
+    "manufacturerID" TEXT,
+
+    CONSTRAINT "Bodywork_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Powertrain" (
+    "id" TEXT NOT NULL,
+    "nameModel" TEXT NOT NULL,
+    "description" TEXT,
+    "serialNumber" TEXT,
+    "year" DOUBLE PRECISION NOT NULL,
+    "fuel" DOUBLE PRECISION NOT NULL,
+    "manufacturerID" TEXT,
+
+    CONSTRAINT "Powertrain_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Manufacturer" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "cnpj" TEXT,
+    "nationality" TEXT NOT NULL,
+
+    CONSTRAINT "Manufacturer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Operator" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "cnpj" TEXT,
+    "nationality" TEXT NOT NULL,
+
+    CONSTRAINT "Operator_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "provider_account_id" TEXT NOT NULL,
+    "refresh_token" TEXT,
+    "access_token" TEXT,
+    "expires_at" INTEGER,
+    "token_type" TEXT,
+    "scope" TEXT,
+    "id_token" TEXT,
+    "session_state" TEXT,
+    "oauth_token_secret" TEXT,
+    "oauth_token" TEXT,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL,
+    "session_token" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "password" TEXT,
+    "email" TEXT,
+    "emailVerified" TIMESTAMP(3),
+    "image" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "NewspaperColumn" (
+    "id" TEXT NOT NULL,
+    "identifier" TEXT NOT NULL,
+
+    CONSTRAINT "NewspaperColumn_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "VerificationToken" (
+    "id" TEXT NOT NULL,
+    "identifier" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "VerificationToken_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_PostImages" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_VehicleImages" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_provider_provider_account_id_key" ON "Account"("provider", "provider_account_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_session_token_key" ON "Session"("session_token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_token_key" ON "VerificationToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_identifier_token_key" ON "VerificationToken"("identifier", "token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_PostImages_AB_unique" ON "_PostImages"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_PostImages_B_index" ON "_PostImages"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_VehicleImages_AB_unique" ON "_VehicleImages"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_VehicleImages_B_index" ON "_VehicleImages"("B");
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_newspaperColumnID_fkey" FOREIGN KEY ("newspaperColumnID") REFERENCES "NewspaperColumn"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Image" ADD CONSTRAINT "Image_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Vehicle" ADD CONSTRAINT "Vehicle_bodyworkID_fkey" FOREIGN KEY ("bodyworkID") REFERENCES "Bodywork"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Vehicle" ADD CONSTRAINT "Vehicle_powertrainID_fkey" FOREIGN KEY ("powertrainID") REFERENCES "Powertrain"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Vehicle" ADD CONSTRAINT "Vehicle_operatorID_fkey" FOREIGN KEY ("operatorID") REFERENCES "Operator"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Bodywork" ADD CONSTRAINT "Bodywork_manufacturerID_fkey" FOREIGN KEY ("manufacturerID") REFERENCES "Manufacturer"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Powertrain" ADD CONSTRAINT "Powertrain_manufacturerID_fkey" FOREIGN KEY ("manufacturerID") REFERENCES "Manufacturer"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Session" ADD CONSTRAINT "Session_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_PostImages" ADD CONSTRAINT "_PostImages_A_fkey" FOREIGN KEY ("A") REFERENCES "Image"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_PostImages" ADD CONSTRAINT "_PostImages_B_fkey" FOREIGN KEY ("B") REFERENCES "Post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_VehicleImages" ADD CONSTRAINT "_VehicleImages_A_fkey" FOREIGN KEY ("A") REFERENCES "Image"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_VehicleImages" ADD CONSTRAINT "_VehicleImages_B_fkey" FOREIGN KEY ("B") REFERENCES "Vehicle"("id") ON DELETE CASCADE ON UPDATE CASCADE;
