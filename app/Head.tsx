d178d794e0b6a228f2940d9816de41d5b92dbaa3
NATANAEL RAZZOTTO <natanael8447@outlook.com>
2024-07-11 23:59:12 -0300

    feat: :zap: Include Google Analytics monitoring

---
+++ app/Head.tsx
@@ -0,0 +1,32 @@
+// app/head.js ou app/Head.tsx
+import Script from 'next/script';
+
+const GA_TRACKING_ID = process.env.GA_TRACKING_ID//'G-XXXXXXXXXX'; // Substitua pelo seu ID de rastreamento
+
+
+export default function Head() {
+
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+    </>
+  );
+}
